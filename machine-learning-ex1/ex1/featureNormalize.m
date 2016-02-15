05df7c6bdfafba26538a79f6c57715f3aa94dd2f
swang143 <swang143@gmail.com>
2016-02-14 19:20:29 -0500

    implement normal equation

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -5,21 +5,11 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   is 1. This is often a good preprocessing step to do when
 %   working with learning algorithms.
 
-% You need to set these values correctly
 X_norm = X;
 mu = mean(X_norm); 
 sigma = std(X_norm);
 
 for j = 1 : size(X_norm,2)
     X_norm(:,j) = (X_norm(:,j) - mu(j)) / sigma(j);
-end
-% First, for each feature dimension, compute the mean
-% of the feature and subtract it from the dataset,
-% storing the mean value in mu. Next, compute the 
-% standard deviation of each feature and divide
-% each feature by it's standard deviation, storing
-% the standard deviation in sigma. 
-%
-% 'mean' and 'std' functions useful.
-     
+end  
 end
